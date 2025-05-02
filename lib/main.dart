99ca1584aa39ef7a6fd23f7129820f8a6a325ddb
Nitesh-Sachde <202412083@daiict.ac.in>
2025-05-02 20:15:25 +0530

    restored

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:sportzy/router/auth_wrapper.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
